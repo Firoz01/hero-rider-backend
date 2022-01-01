7c82eefb8f263a17efaa788d793063ff8542358a
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2022-01-01 21:36:42 +0600

    add cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -9,7 +9,7 @@ const learnerRouter = require('./routes/learnerRoute');
 const adminRouter = require('./routes/adminRoute');
 
 const app = express();
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
