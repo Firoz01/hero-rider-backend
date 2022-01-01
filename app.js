81bfa5bbaaa452725b8eed6c6a70a160e0bf3712
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2022-01-01 20:53:58 +0600

    error in heroku

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const learnerRouter = require('./routes/learnerRoute');
 const adminRouter = require('./routes/adminRoute');
 
 const app = express();
+
 app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
